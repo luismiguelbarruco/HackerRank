043c7231ef957ac99826a251a0cdf928d7d16e36
luis.m.barruco <luis.m.barruco@hotmail.com>
2020-05-03 14:34:01 -0300

    new problems solved

---
+++ Interview Preparation Kit/Array/2D_Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2D_Array
+{
+    class Program
+    {
+        static int sunGlass(int[][] arr, int i, int j)
+        {
+            return arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                arr[i + 1][j + 1] + 
+                arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+        }
+
+        static int hourglassSum(int[][] arr) 
+        {
+            var total = int.MinValue;
+            for(var i = 0; i < 4; i++)
+            {
+                for(var j = 0; j < 4; j++)
+                {
+                    var result = sunGlass(arr, i, j);
+                    if(result > total)
+                        total = result;
+                }   
+            }
+
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] mat = new int[][]
+            {
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 1, 0, 0, 0, 0 },
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 0, 2, 4, 4, 0 },
+                new int[] { 0, 0, 0, 2, 0, 0 },
+                new int[] { 0, 0, 1, 2, 4, 0 }
+            };
+
+            hourglassSum(mat);
+        }
+    }
+}
---
+++ Interview Preparation Kit/Array/left_rotation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace left_rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = new int[] {1, 2, 3, 4, 5};
+            var input = new int[] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 };
+            var result = rotLeft(input, 13);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static int[] rotLeft(int[] a, int d) 
+        {
+            int[] arr = new int[a.Length];
+            for(var i = a.Length - 1; i >= 0; i--)
+            {
+                var newIndex = (i - d);
+                if(newIndex < 0)
+                    newIndex += a.Length;
+
+                arr[newIndex] = a[i];
+            }
+
+            return arr;
+        }
+    }
+}
